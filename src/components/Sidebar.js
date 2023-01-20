500bef9e54f77a13094f12751a83afb1fca47f21
Ivanna Liuba <i.ivanna.liuba@gmail.com>
2023-01-20 17:47:53 +0100

    added sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+//import {Avatar} from "@mui/material";
+
+const Sidebar = () => {
+    return (
+        <Container>
+            <Header>
+
+
+            </Header>
+        </Container>
+    )
+}
+
+export default Sidebar
+
+
+const Container = styled.div``;
+const Header = styled.div``;
+/*
+const UserAvatar = styled(Avatar)`
+    margin: 10px;
+`;*/
